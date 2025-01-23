82ab143f570255c28bc865a59325302c3d23f962
Akshay Dhumal <129518293+Asdhumal09@users.noreply.github.com>
2025-01-24 01:14:16 +0530

    create buttons eoffice, apalesarkar etc. (#7)
    Co-authored-by: Akshay Dhumal <akshaydhumal.ek@gmail.com>

---
+++ src/pages/ApaleSarkarTable.js
@@ -0,0 +1,253 @@
+import React from "react";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  Typography,
+  Box,
+} from "@mui/material";
+import TopBar from "./TopBar";
+
+const data = [
+  {
+    office: "Collector office (Revenue), Jalna",
+    totalComplaints: 22,
+    resolved: 22,
+    pending: 0,
+    pendingPercent: "0%",
+  },
+  {
+    office: "Sub-Divisional Office (Revenue), Ambad",
+    totalComplaints: 65,
+    resolved: 11,
+    pending: 7,
+    pendingPercent: "17%",
+  },
+  {
+    office: "Sub-Divisional Office (Revenue), Jalna",
+    totalComplaints: 133,
+    resolved: 107,
+    pending: 7,
+    pendingPercent: "7%",
+  },
+  {
+    office: "Sub-Divisional Office (Revenue), Bhokardan",
+    totalComplaints: 87,
+    resolved: 11,
+    pending: 3,
+    pendingPercent: "13%",
+  },
+  {
+    office: "Sub-Divisional Office (Revenue), Partur",
+    totalComplaints: 72,
+    resolved: 57,
+    pending: 1,
+    pendingPercent: "1%",
+  },
+  {
+    office: "Taluka Office (Revenue), Ambad",
+    totalComplaints: 15,
+    resolved: 14,
+    pending: 1,
+    pendingPercent: "7%",
+  },
+  {
+    office: "Taluka Office (Revenue), Badnapur",
+    totalComplaints: 10,
+    resolved: 7,
+    pending: 3,
+    pendingPercent: "30%",
+  },
+  {
+    office: "Taluka Office (Revenue), Bhokardan",
+    totalComplaints: 29,
+    resolved: 29,
+    pending: 0,
+    pendingPercent: "0%",
+  },
+  {
+    office: "Taluka Office (Revenue), Ghansawangi",
+    totalComplaints: 12,
+    resolved: 12,
+    pending: 0,
+    pendingPercent: "0%",
+  },
+  {
+    office: "Taluka Office (Revenue), Jafrabad",
+    totalComplaints: 23,
+    resolved: 22,
+    pending: 1,
+    pendingPercent: "4%",
+  },
+  {
+    office: "Taluka Office (Revenue), Jalna",
+    totalComplaints: 11,
+    resolved: 11,
+    pending: 0,
+    pendingPercent: "0%",
+  },
+  {
+    office: "Taluka Office (Revenue), Mantha",
+    totalComplaints: 65,
+    resolved: 11,
+    pending: 7,
+    pendingPercent: "17%",
+  },
+  {
+    office: "Taluka Office (Revenue), Partur",
+    totalComplaints: 3,
+    resolved: 3,
+    pending: 0,
+    pendingPercent: "0%",
+  },
+  {
+    office: "Total",
+    totalComplaints: 365,
+    resolved: 339,
+    pending: 26,
+    pendingPercent: "7%",
+  },
+];
+
+const tableStyles = {
+  container: { maxWidth: "1000px", margin: "20px auto" },
+  header: {
+    padding: "10px",
+    fontWeight: "bold",
+    backgroundColor: "#f5f5f5",
+    fontSize: "35px",
+  },
+  table: { border: 1, borderColor: "#ccc" },
+  cell: { fontSize: "16px", padding: "8px", border: 1, borderColor: "#ccc" },
+};
+
+const ApaleSarkarTable = () => {
+  return (
+    <Box className="mt-20">
+      <TopBar />
+      <TableContainer component={Paper} sx={tableStyles.container}>
+        <Typography
+          variant="h6"
+          align="center"
+          className="ff_yatra"
+          sx={{ fontSize: "35px", backgroundColor: "#f5f5f5", padding: "10px" }}
+        >
+         Aaple Sarkar
+        </Typography>
+
+        <Table>
+          <TableHead sx={{ backgroundColor: "#f0f0f0" }}>
+            <TableRow>
+              {[
+                "Sr No",
+                "काययालय",
+                "एकूण तक्रारी",
+                "निस्कारीत केले",
+                "प्रलंबित",
+                "प्रलंबित %",
+              ].map((header, index) => (
+                <TableCell
+                  key={index}
+                  align={index === 0 ? "center" : index === 1 ? "left" : "center"}
+                  sx={{ ...tableStyles.cell, fontSize: "20px" }}
+                  className="ff_yatra"
+                >
+                  {header}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {data.map((row, index) => (
+              <TableRow
+                key={index}
+                sx={
+                  row.office === "Total"
+                    ? { backgroundColor: "#fff" }
+                    : null
+                }
+              >
+                <TableCell
+                  align="center"
+                  sx={{ ...tableStyles.cell}}
+                className="ff_baloo">
+                  {index + 1}
+                </TableCell>
+                <TableCell
+                  sx={{
+                    ...tableStyles.cell,
+                    fontFamily:
+                      row.office === "Total"
+                        ? "YatraOne, sans-serif"
+                        : "Baloo, sans-serif",
+                  }}
+                  className={row.office === "Total" ? "ff_yatra" : "ff_baloo"}
+                >
+                  {row.office}
+                </TableCell>
+                <TableCell
+                  align="center"
+                  sx={{
+                    ...tableStyles.cell,
+                    fontFamily:
+                      row.office === "Total"
+                        ? "YatraOne, sans-serif"
+                        : "Baloo, sans-serif",
+                  }}
+                  className={row.office === "Total" ? "ff_yatra" : "ff_baloo"}
+                >
+                  {row.totalComplaints}
+                </TableCell>
+                <TableCell
+                  align="center"
+                  sx={{
+                    ...tableStyles.cell,
+                    fontFamily:
+                      row.office === "Total"
+                        ? "YatraOne, sans-serif"
+                        : "Baloo, sans-serif",
+                  }}
+                  className={row.office === "Total" ? "ff_yatra" : "ff_baloo"}
+                >
+                  {row.resolved}
+                </TableCell>
+                <TableCell
+                  align="center"
+                  sx={{
+                    ...tableStyles.cell,
+                    fontFamily:
+                      row.office === "Total"
+                        ? "YatraOne, sans-serif"
+                        : "Baloo, sans-serif",
+                  }}
+                  className={row.office === "Total" ? "ff_yatra" : "ff_baloo"}
+                >
+                  {row.pending}
+                </TableCell>
+                <TableCell
+                  align="center"
+                  sx={{
+                    ...tableStyles.cell,
+                    fontFamily:
+                      row.office === "Total"
+                        ? "YatraOne, sans-serif"
+                        : "Baloo, sans-serif",
+                  }}
+                  className={row.office === "Total" ? "ff_yatra" : "ff_baloo"}
+                >
+                  {row.pendingPercent}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Box>
+  );
+};
+
+export default ApaleSarkarTable;
